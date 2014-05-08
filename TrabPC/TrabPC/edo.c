72f2305e9ea29a549f8fbf3650718ac79996ddf4
jlessa <jefferson2459@gmail.com>
2014-05-08 19:07:42 -0300

    Update edo.c
    rungekuttaterceiraordem

--- TrabPC/TrabPC/edo.c
+++ TrabPC/TrabPC/edo.c
@@ -32,10 +32,10 @@ void preditorCorretor(float x0,float y0,int h,int m){
 	// Calculo de x[0]->x[3] e y[0]->y[3] pelo metodo RK4
 	float * x = malloc(sizeof(float)* (m + 1));
 	float * y = malloc(sizeof(float)* (m + 1));
-	
+
 	x[0] = x0;
 	y[0] = y0;
-	
+
 	// calculo de x[0] -> x[3] e y[0] -> y[3] pelo metodo de Runge Kutta 
 	for(j = 0; j < 3; j++) {
 		k1 = f(x[j], y[j]);
@@ -45,18 +45,18 @@ void preditorCorretor(float x0,float y0,int h,int m){
 		y[j + 1] = y[j] + (h/6)*(k1 + 2*k2 + 2*k3 + k4);
 		x[j + 1] = x[j] + h;
 	}
-	
+
 	float yp, fp;
-	
+
 	// Calculo da EDO usando o metodo Preditor-Corretor 
 	for(j = 3; j < m; j++) {
 		x[j+1] = x[j] + h;
 		yp = y[j] + (h/24)*(55*f(x[j], y[j]) - 59*f(x[j-1], y[j-1]) + 37*f(x[j-2], y[j-2]) - 9*f(x[j-3], y[j-3]));
 		fp = y[j] + (h/24)*(9*f(x[j+1], yp) + 19*f(x[j], y[j]) - 5*f(x[j-1], y[j-1]) + f(x[j-2], y[j-2]));
 		y[j+1] = y[j] + (h/24)*(9*f(x[j+1], fp) + 19*f(x[j], y[j]) - 5*f(x[j-1], y[j-1]) + f(x[j-2], y[j-2]));
-		
+
 	}
-	
+
 	// 
 	printf("Os valores de x e y sao: \n");
 	for(j = 0; j <= m; j++) {
@@ -69,10 +69,10 @@ void rungeKuttaQuartaOrdem(float x0,float y0,int h,int m){
 	float * x = malloc(sizeof(float)* (m + 1));
 	float * y = malloc(sizeof(float)* (m + 1));;
 	float k1, k2, k3, k4;
-	
+
 	x[0] = x0;
 	y[0] = y0;
-	
+
 	for(j = 0; j < m; j++) {
 		k1 = f(x[j], y[j]);
 		k2 = f(x[j] + h/2, y[j] + (h/2)*k1);
@@ -81,24 +81,47 @@ void rungeKuttaQuartaOrdem(float x0,float y0,int h,int m){
 		y[j + 1] = y[j] + (h/6)*(k1 + 2*k2 + 2*k3 + k4);
 		x[j + 1] = x[j] + h;
 	}
-	
+
 	// Mostra os valores
 	printf("Os valores de x e y sao: \n"); 
 	for(j = 0; j <= m; j++) {
 		printf("%f, %f \n", x[j], y[j]);
 	} 
+
+};
+
+void rungeKuttaTerceiraOrdem(float x0,float y0,int h,int m){	
+	float k1, k2, k3;
+	int  j;
 	
-}
+	float x[m+1], y[m+1];
+	x[0] = x0;
+	y[0] = y0;
+	
+	// faz os calculos 
+	for(j = 0; j < m; j++) {
+		k1 = f(x[j], y[j]);
+		k2 = f(x[j] + h/2, y[j] + k1/2);
+		k3 = f(x[j] + ((float)3/4)*h, y[j] + ((float)3/4)*k2);
+		x[j+1] = x[j] + h;
+		y[j+1] = y[j] + (h/9)*(2*k1 + 3*k2 + 4*k3); 
+	}
+	
+	printf("Os valores de x e y sao:\n");
+	for(j = 0; j <= m; j++) {
+		printf("%f, %f; \n",x[j], y[j]);
+	}	
+};
 
 void rungeKuttaSegundaOrdem(float x0,float y0,int h,int m){
 	int j;		
 	float * x = malloc(sizeof(float)* (m + 1));
 	float * y = malloc(sizeof(float)* (m + 1));
 	float k1, k2;
-	
+
 	x[0] = x0;
 	y[0] = y0;
-	
+
 	//Calculo pelo metodo de Runge Kutta de segunda ordem 
 	for(j = 0; j < m; j++) {
 		k1 = f(x[j], y[j]);
@@ -106,10 +129,10 @@ void rungeKuttaSegundaOrdem(float x0,float y0,int h,int m){
 		y[j + 1] = y[j] + (h/2)*(k1 + k2);
 		x[j + 1] = x[j] + h;
 	}
-	
+
 	//mostra os resultados
 	printf("Os valores de x e y sao: \n");
 	for(j = 0; j <= m; j++) {
 		printf("%.4f, %.4f \n", x[j], y[j]);
 	}
-}
+};
