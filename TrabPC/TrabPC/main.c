c586ecef072d83b9ab232445e31ee0c553d431e4
jlessa <jefferson2459@gmail.com>
2014-05-08 20:35:04 -0300

    Update main.c
    Pronto! Falta interface

--- TrabPC/TrabPC/main.c
+++ TrabPC/TrabPC/main.c
@@ -1,10 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "edo.h"
 
-int main(void){
-	lua_State * L = luaL_newstate();
+int main(void){	
 	float x0, y0, h;
-	int m,entrada;
-
+	int m,entrada,id;
+	
+	//escolha da funcao
+	printf("Digite qual funcao deseja usar: \n");
+	printf("(1) - (1 - x + 4*y) \n");
+	printf("(2) - (1 - y/x) \n");
+	printf("(3) - (x - y + 2) \n");
+	printf("(4) - (-10 * y) \n");
+	printf("(5) - (0.04 * y) \n");
+	scanf("%d", &id);	
+	
 	//entrada de dados
 	printf("Digite o valor de x inicial: \n");
 	scanf("%f", &x0);
@@ -26,15 +36,15 @@ int main(void){
 	switch (entrada)
 	{
 	case 1:
-		metodoEuler(x0,y0,h,m);
+		metodoEuler(x0,y0,h,m,id);
 	case 2:
-		rungeKuttaSegundaOrdem(x0,y0,h,m);
+		rungeKuttaSegundaOrdem(x0,y0,h,m,id);
 	case 3:
-		rungeKuttaTerceiraOrdem(x0,y0,h,m);	
+		rungeKuttaTerceiraOrdem(x0,y0,h,m,id);	
 	case 4:
-		rungeKuttaQuartaOrdem(x0,y0,h,m);
+		rungeKuttaQuartaOrdem(x0,y0,h,m,id);
 	case 5:
-		preditorCorretor(x0,y0,h,m);
+		preditorCorretor(x0,y0,h,m,id);
 	default:
 		break;
 	}
