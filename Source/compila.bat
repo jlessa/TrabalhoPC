8335015b1209784fa799e56d833f4649855379b6
Carlos <carlos.oliv@gmail.com>
2014-05-09 19:04:53 -0300

    tinha um bug nessa porra!

--- Source/compila.bat
+++ Source/compila.bat
@@ -1,11 +1,11 @@
 del t1.exe
-<<<<<<< HEAD
+
 gcc -c main.c edo.c -I ./include -I ./dependencies/include -static-libgcc -static-libstdc++
 gcc -o t1.exe -L./lib/ -llua51 -liup -liuplua51 -liupluacontrols51 -lgdi32 -lcomdlg32 -lcomctl32 -luuid -loleaut32 -lole32 edo.o main.o -static-libgcc -static-libstdc++
-=======
+
 gcc -c main.c edo.c -I ./include -I ./dependencies/include
 gcc -o t1.exe -lmsvcrt -L./lib/ -llua51 -liup -liuplua51 -liupluacontrols51 -lgdi32 -user32 -lcomdlg32 -lcomctl32 -luuid -loleaut32 -lole32 edo.o main.o 
->>>>>>> 90dcdce2bbb8d66e9ad86c0820cba9c5f16173b4
+
 del main.o
 del edo.o
 t1.exe
