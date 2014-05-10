7aa9716379fb6c4c3167da86a9b0a932f506f1f6
Carlos <carlos.oliv@gmail.com>
2014-05-10 01:51:56 -0300

    testes com scripts
    essa porra ta me dando uma raiva

--- Source/compila-bom.bat
+++ Source/compila-bom.bat
@@ -4,7 +4,8 @@ gcc -Wall -x c++ -Wno-unused-value -std=c++98 -pedantic  -g    -I"C:\Program Fil
 gcc -o t1.exe -L./lib/ -llua51 -liup -liuplua51 -liupluacontrols51 -lgdi32 -lcomdlg32 -lcomctl32 -luuid -loleaut32 -lole32 edo.o main.o -static-libgcc -static-libstdc++
 
 gcc -c main.c edo.c -I ./include -I ./dependencies/include
-gcc -L"C:\Program Files (x86)\CodeBlocks-EP\sdk\koolplot\lib" -L"C:\Program Files (x86)\CodeBlocks-EP\sdk\winbgim\lib" -o t1.exe -lmsvcrt -L./lib/ -llua51 -liup -liuplua51 -liupluacontrols51 -lgdi32 -lcomdlg32 -lcomctl32 -luuid -loleaut32 -lole32  -lkoolplot -lbgi -lgdi32 -lcomdlg32 -luuid -loleaut32 -lole32 -lole32 edo.o main.o 
+gcc -o t1.exe -lmsvcrt -L./lib/ -llua51 -liup -liuplua51 -liupluacontrols51 -lgdi32 -user32 -lcomdlg32 -lcomctl32 -luuid -loleaut32 -lole32 edo.o main.o 
 
-
+del main.o
+del edo.o
 t1.exe
