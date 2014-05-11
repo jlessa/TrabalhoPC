9b400b20262f1b17ae78769e59cc7334dc3bf849
Carlos <carlos.oliv@gmail.com>
2014-05-11 06:55:49 -0300

    trabalho do codeblocks
    trabalho do codeblocks

--- Source/compila.bat
+++ Source/compila.bat
@@ -1,14 +1,8 @@
 del t1.exe
-<<<<<<< HEAD
-
-gcc -Wall -x c++ -Wno-unused-value -std=c++98 -pedantic  -g    -I"C:\Program Files (x86)\CodeBlocks-EP\sdk\koolplot\include" -I"C:\Program Files (x86)\CodeBlocks-EP\sdk\winbgim\include" -c main.c edo.c -I ./include -I ./dependencies/include -static-libgcc -static-libstdc++
-gcc -o t1.exe -L./lib/ -llua51 -liup -liuplua51 -liupluacontrols51 -lgdi32 -lcomdlg32 -lcomctl32 -luuid -loleaut32 -lole32 edo.o main.o -static-libgcc -static-libstdc++
 
 gcc -c main.c edo.c -I ./include -I ./dependencies/include
-gcc -L"C:\Program Files (x86)\CodeBlocks-EP\sdk\koolplot\lib" -L"C:\Program Files (x86)\CodeBlocks-EP\sdk\winbgim\lib" -o t1.exe -lmsvcrt -L./lib/ -llua51 -liup -liuplua51 -liupluacontrols51 -lgdi32 -lcomdlg32 -lcomctl32 -luuid -loleaut32 -lole32  -lkoolplot -lbgi -lgdi32 -lcomdlg32 -luuid -loleaut32 -lole32 -lole32 edo.o main.o 
+gcc -o t1.exe -lmsvcrt -L./lib/ -llua51 -liup -liuplua51 -liupluacontrols51 -lgdi32 -user32 -lcomdlg32 -lcomctl32 -luuid -loleaut32 -lole32 edo.o main.o 
 
-
-=======
-gcc -I ./include -I ./dependencies/include -static -o t1.exe edo.c main.c -L./lib/ -llua5.1
->>>>>>> 58b95e5d7e86ababa1b576171cecc0e9b2dbb0dd
+del main.o
+del edo.o
 t1.exe
