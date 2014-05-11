46c377fa6b298724bbb284052e537bb2b493c437
Carlos <carlos.oliv@gmail.com>
2014-05-11 06:57:40 -0300

    script compila
    script para compilar

--- Source/compila_antigo.bat
+++
@@ -1,3 +0,0 @@
-del t1.exe
-gcc -I ./include -I ./dependencies/include -static -o t1.exe edo.c main.c -lmsvcrt -L./lib/ -llua5.1 -liup.dll -liuplua51.dll -liupluacontrols51.dll -lgdi32 -user32 -lcomdlg32 -lcomctl32 -luuid -loleaut32 -lole32
-t1.exe
